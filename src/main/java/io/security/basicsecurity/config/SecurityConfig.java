287e45b0df9ece184f1d4a209884309cf166ed2c
DoosicEE <strou73@naver.com>
2022-06-18 09:21:56 +0900

    #1- 사용자 정의 보안 기능 구현
    Config 파일을 통해 사용자가 직접 어떻게 인증과 인가를 처리할지 설정하는 방법을 배웠다.

--- src/main/java/io/security/basicsecurity/config/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -1,2 +1,23 @@
-package io.security.basicsecurity.config;public class SecurityConfig {
+package io.security.basicsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 인증 API 와 인가 API 에 대해서 추가적으로 설정할 수 있는 메서드
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        // 인가정책 - 어떠한 경우에도 인증을 받아야한다.
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+        // 인증정책 - formLogin 방식으로 인증을 받는다.
+        http
+                .formLogin();
+    }
 }
